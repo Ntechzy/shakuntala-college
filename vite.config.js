d6b8e1b40e9afd5944470cdc5dda93464a9b95e5
pankaj upadhyay <pankajup.git01@gmail.com>
2024-05-08 17:57:15 +0530

    route updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.{jpg,jpeg,png,gif,svg,ico,eot,otf,ttf,woff,woff2,mp4,webm,wav,mp3,m4a,aac,oga}'],
+  assetsInclude: ['**/*.{jpg,jpeg,png,gif,svg,ico,eot,otf,ttf,woff,woff2,mp4,webm,wav,mp3,m4a,aac,oga,JPG}'],
 });
