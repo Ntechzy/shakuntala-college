575a7bb210fb6ad0124c97ef953c2f342de3ecc8
pankaj upadhyay <pankajup.git01@gmail.com>
2024-05-08 14:27:10 +0530

    vercel file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Home from "./pages/Home/Home";
+import Root from './routes/Root'
+import ErrorPage from './error-page'
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        }
+
+      ],
+    },
+  ]);
+
   return (
     <div>
-      <Home />
+      <RouterProvider router={router} />
+
     </div>
   );
 };
