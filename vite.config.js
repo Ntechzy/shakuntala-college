107c25817728f621e5d247c1168b0b94652af4ea
Siddharth Singh <siddharth.singh19@outlook.com>
2024-05-09 17:46:54 +0530

    finally

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "http://drshakuntalaayush.in/",
+  base: "http://drshakuntalaayush.in/",
   plugins: [react()],
   assetsInclude: [
     "**/*.{jpg,jpeg,png,gif,svg,ico,eot,otf,ttf,woff,woff2,mp4,webm,wav,mp3,m4a,aac,oga,JPG}",
