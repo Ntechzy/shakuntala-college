29db72adbbbdb305d3cadd31857ba755de743c79
vikasntechzy <vikas@ntechzy.in>
2025-01-21 15:49:16 +0530

    add link

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://drshakuntalaayush.in/" || "https://shakuntala-college.vercel.app/",
+  base: "https://shakuntala-college.vercel.app/",
   plugins: [react()],
   assetsInclude: [
     "**/*.{jpg,jpeg,png,gif,svg,ico,eot,otf,ttf,woff,woff2,mp4,webm,wav,mp3,m4a,aac,oga,JPG}",
