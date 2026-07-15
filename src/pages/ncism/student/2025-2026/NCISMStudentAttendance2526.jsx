0e02abdb85ac861de424b2a2edf7cfa92f6c69ed
Devansh <devkulsh1308@gmail.com>
2026-07-15 16:22:26 +0530

    Refactor attendance components to include DocumentViewer for 2024-2025 and 2025-2026

--- src/pages/ncism/student/2025-2026/NCISMStudentAttendance2526.jsx
+++ src/pages/ncism/student/2025-2026/NCISMStudentAttendance2526.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ExcelViewer from '../../../../shared/ExcelViewer'
+import React from "react";
+import ExcelViewer from "../../../../shared/ExcelViewer";
+import DocumentViewer from "../../../../shared/DocumentViewer";
 
 const NCISMStudentAttendance2526 = () => {
   return (
-    <ExcelViewer title="Student Attendance (Academic Year 2025–2026)" fileUrl="https://res.cloudinary.com/dhlqc0ymy/raw/upload/v1772087846/studentAttendance25-26_wmxegg.xlsx" />
-  )
-}
+    <>
+      <DocumentViewer
+        title="Student Attendance (Academic Year 2025–2026)"
+        fileUrl="https://res.cloudinary.com/dhlqc0ymy/image/upload/v1784112712/2025-26_batch_adwl4v.pdf"
+      />
+      <ExcelViewer
+        title="Student Attendance (Academic Year 2025–2026)"
+        fileUrl="https://res.cloudinary.com/dhlqc0ymy/raw/upload/v1772087846/studentAttendance25-26_wmxegg.xlsx"
+      />
+    </>
+  );
+};
 
-export default NCISMStudentAttendance2526
+export default NCISMStudentAttendance2526;
